ffb1298d1a06a4af4ad079febcc11407fe830dfc
sfr2645 <sfr2645@email.vccs.edu>
2024-09-18 15:41:25 +0000

    added code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
+//This project asks the user how many cookies he or she wants to make, then displays the number of cups of each ingredient needed for the specified number of cookies.
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
-int main() { std::cout << "Hello World!\n"; }
+using namespace std;
+
+int main()
+
+{
+  double sugar, butter, flour, cookies;
+  cout << "How many cookies would you like to make? ";
+  cin >> cookies;
+  sugar=(cookies/48)*1.5;
+  butter=(cookies/48);
+  flour=(cookies/48)*2.75;
+  cout << "To make " << cookies << " cookies, you will need:\n" << sugar << "cup(s) of sugar \n" << butter << "cup(s) of butter \n" << flour << "cup(s) of flour";
+}
